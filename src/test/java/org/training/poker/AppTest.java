07fb2d2e8425b65303724ee38fa003c44a6bcaa7
Si6Ba <48552955+Si6Ba@users.noreply.github.com>
2019-03-15 15:22:04 +0200

    create card

--- src/test/java/org/training/poker/AppTest.java
+++ src/test/java/org/training/poker/AppTest.java
@@ -1,5 +1,6 @@
 package org.training.poker;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -7,14 +8,16 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+public class AppTest {
+	
+	@Test
+	public void canCreateCard() {
+
+		Card card = new Card("5C");
+
+		assertEquals(card.rank, Rank.FIVE);
+		assertEquals(card.suit, Suit.C);
+
+	}
+
 }
